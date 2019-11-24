08c0405b3d6eadfa0ef561c5618e2feed72ba832
suphit <43198144+suphit@users.noreply.github.com>
2019-11-24 19:01:43 +0700

    Add files via upload

--- bai5b.py
+++ bai5b.py
@@ -5,12 +5,9 @@ Created on Sun Nov 24 08:39:16 2019
 @author: Suphit
 """
 
-import random
 import numpy as np
 import matplotlib.pyplot as plt
-x,y = np.meshgrid(np.arange(8),np.arange(8)) 
-chieucao = np.random.randint(0,64,(8,8)) 
-plt.gca(aspect=1,xlim=[-1,8],ylim=[-1,8])
-plt.scatter(x,y,c=chieucao,s=600,marker='s',cmap='RdPu') 
-plt.colorbar()
+x = np.arange(64).reshape(8,8)
+plt.figure(figsize=[8,8])
+plt.imshow(x,cmap='RdPu')
 plt.show()
